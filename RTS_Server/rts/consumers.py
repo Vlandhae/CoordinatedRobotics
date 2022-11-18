8132786a60418a33b40d31a88f0fde78d28fda9c
Gaston Magnin <gaston@magnin.de>
2022-11-18 19:03:12 +0000

    consumers now work with sessions

--- RTS_Server/rts/consumers.py
+++ RTS_Server/rts/consumers.py
@@ -2,15 +2,15 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from time import sleep
-from rts.models import Car
+from rts.models import Car, CarSession
 from rts.serializers import CarSerializer
-import rts.car_data
+from rts.constants import GROUP_CLOSING_COMMAND, COMMAND_LIST
 
 class CarConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["car_name"]
         self.room_group_name = f"group_{self.room_name}"
-
+        print(self.room_group_name)
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
@@ -18,6 +18,10 @@ class CarConsumer(WebsocketConsumer):
 
         try: 
             self.car = Car.objects.get(name=self.room_name)                
+            print(f"{self.room_name} : {self.channel_name}")
+            # TODO this shoudl only happen when the car joins the channel maybe specific message
+            self.car.channel_name = self.channel_name
+            self.car.save()
         except Car.DoesNotExist:
             pass
 
@@ -30,8 +34,16 @@ class CarConsumer(WebsocketConsumer):
         )
         print(f"disconnected {close_code}")
     
+    def move(self, newChannel):
+        # TODO find out why no messages are received in the new group
+        async_to_sync(self.channel_layer.group_add)(
+            newChannel, self.channel_name
+        )
+        self.send("moved succesfully")
+
     def receive(self, text_data):
         msg = text_data
+        print(msg)
         if msg == "info":
             if self.car is None:
                 self.send("car does not exist")
@@ -39,19 +51,72 @@ class CarConsumer(WebsocketConsumer):
                 serializer = CarSerializer(self.car)
                 self.send(json.dumps(serializer.data))
             return
+        if msg=="move":
+            self.move("group_car3")
+        if msg == GROUP_CLOSING_COMMAND:
+            async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
         print(msg)
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "command", "command": msg}
-        )        
+        )    
+    def system(self, event):
+        print("received system command")
 
     def command(self, event):
-
         print(event)
         print("command event received")
         com = event.get("command", None)
         if com is None:
             return        
-        if com.upper() in rts.car_data.command_list and self.car is not None:
+        if com.upper() in COMMAND_LIST and self.car is not None:
             self.car.current_command = com.upper()
             self.car.save()
         self.send(text_data=json.dumps({"command":com}))
+
+class SessionConsumer(WebsocketConsumer):
+    def connect(self):
+        # TODO add different rout for this and replace car_name
+        self.room_name = self.scope["url_route"]["kwargs"]["session_name"]
+        self.room_group_name = f"group_{self.room_name}"
+        print(self.room_group_name)
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+    def command(self, event):
+        print(event)
+        print("command event received")
+        com = event.get("command", None)
+        if com is None:
+            return        
+        if com.upper() in COMMAND_LIST and self.car is not None:
+            self.car.current_command = com.upper()
+            self.car.save()
+        self.send(text_data=json.dumps({"command":com}))
+
+    def receive(self, text_data):
+        msg = text_data
+        print(msg)
+        if msg == "info":
+            if self.car is None:
+                self.send("car does not exist")
+            else:
+                serializer = CarSerializer(self.car)
+                self.send(json.dumps(serializer.data))
+            return
+        if msg=="move":
+            self.move("group_car3")
+        if msg == GROUP_CLOSING_COMMAND:
+            async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+        print(msg)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "command", "command": msg}
+        )  
+
+    def disconnect(self, close_code):
+         # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+        print(f"disconnected {close_code}")
--- RTS_Server/rts/models.py
+++ RTS_Server/rts/models.py
@@ -7,6 +7,8 @@ class Somedata(models.Model):
 class Car(models.Model):
     name = models.TextField(primary_key = True)
     current_command = models.CharField(max_length=1)
+    # this has to be cleared whenever the car session disconnects
+    channel_name = models.TextField(default="")
 
 
 class CarSession(models.Model):
