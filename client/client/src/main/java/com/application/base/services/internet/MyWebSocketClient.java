92eecd0dd4a1a622762c347de54d6a9930928ba2
Gaston <gaston@magnin.de>
2022-12-27 18:18:07 +0100

    made WebSocketClient actually do something

--- client/client/src/main/java/com/application/base/services/internet/MyWebSocketClient.java
+++ client/client/src/main/java/com/application/base/services/internet/MyWebSocketClient.java
@@ -1,11 +1,68 @@
 package com.application.base.services.internet;
 
-public class MyWebSocketClient {
+import com.application.base.services.actionlog.ActionLogContentService;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
 
-    public MyWebSocketClient() {
+import java.net.URI;
+
+public class MyWebSocketClient extends WebSocketClient{
+    private ActionLogContentService actionLog;
+
+    public MyWebSocketClient(URI serverUri) {
+      super(serverUri);
+    }
+    public MyWebSocketClient(URI serverUri, ActionLogContentService actionLog) {
+      super(serverUri);
+      this.actionLog = actionLog;
+    }
+
+    @Override
+    public void connect()
+    {
+      if(actionLog != null)
+        actionLog.addAdditionalContent("connecting " + this.uri);
+      super.connect();
+    }
+
+    //TODO replace with good code
+    public void setActionLog(ActionLogContentService actionLog)
+    {
+      this.actionLog = actionLog;
+    }
+
+    @Override
+    public void onMessage(String message) {
+      System.out.println("received message " + message);
+      if(this.actionLog != null)
+      {
+        this.actionLog.addAdditionalContent("Received from ws: " + message);
+      }
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshake) {
+      System.out.println("connected to server");
+      this.actionLog.addAdditionalContent("connected");
+      if(this.actionLog != null)
+      {
+        this.actionLog.addAdditionalContent("connected");
+      }
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+    }
+
+    @Override
+    public void onError(Exception ex) {
+      ex.printStackTrace();
     }
 
     public void openConnection(String internetAddress) {
+      connect();
     }
 
     public void closeConnection() {
@@ -15,5 +72,6 @@ public class MyWebSocketClient {
     }
 
     public void sendCommand(String command) {
+      send(command);
     }
 }
